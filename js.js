9ba3b5e7cda053985b0aa7d94cb93f9df6bfb16c
Primoz Babsek <primoz969@gmail.com>
2017-12-02 10:12:27 +0100

    finishes project

--- js.js
+++ js.js
@@ -1,33 +1,4 @@
-// ------------AVG Price Per City -----------
 
-// var amsterdam = [1500, 500, 1800, 700];
-// var london = [2500, 1500, 1000, 900];
-
-// var avgAmsterdam = (amsterdam[0] + amsterdam[1] + amsterdam[2] + amsterdam[3]) / 4; 
-
-// var avgLondon = (london[0] + london[1] + london[2] + london[3]) / 4;
-
-// console.log(avgAmsterdam);
-// console.log(avgLondon);
-
-// -----------------AVG Price Per City As FUNCTION------------------
-
-
-// function amsterdam (price1, price2, price3, price4) {
-// 	var avgAmsterdam = (price1 + price2 + price3 + price4) / 4;
-// 	return avgAmsterdam;
-
-// }
-
-// console.log (amsterdam (1500, 500, 1800, 700));
-
-// function london (price1, price2, price3, price4) {
-// 	var avgLondon = (price1 + price2 + price3 + price4) / 4;
-// 	return avgLondon;
-
-// }
-
-// console.log (london (2500, 1500, 1000, 900));
 
 // ---------Below: avgPrice per City using arrays + Function---------
 
@@ -42,14 +13,14 @@ function averagePrice (price1, price2, price3, price4) {
 
 
 var avgPAmsterdam = averagePrice (pricesAm[0], pricesAm[1], pricesAm[2], pricesAm[3]);
-document.getElementById("avgPrice1").innerHTML = avgPAmsterdam;
+document.getElementById("avgPrice1").innerHTML = avgPAmsterdam + "&euro;";
 
 
 var avgPLondon = averagePrice (pricesLo[0], pricesLo[1], pricesLo[2], pricesLo[3]);
-document.getElementById("avgPrice2").innerHTML = avgPLondon; 
+document.getElementById("avgPrice2").innerHTML = avgPLondon + "&euro;"; 
 
 
-// names of city stored in variables 
+// names of cities stored in variables 
 
 var amsterdam = "Amsterdam";
 var london = "London";
